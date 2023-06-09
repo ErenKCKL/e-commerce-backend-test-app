fe3df96901f0ca779875e876c2cb0c725f7c849f
Eren <eren092003@gmail.com>
2023-06-09 16:53:13 +0200

    start project

---
+++ src/main/java/ashina/ecommerce/entities/concretes/Product.java
@@ -0,0 +1,38 @@
+package ashina.ecommerce.entities.concretes;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int id;
+    
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "quantity_per_unit")
+    private String quantityPerUnit;
+
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+    @Column(name = "unit_in_Stock")
+    private int unitInStock;
+    
+}
